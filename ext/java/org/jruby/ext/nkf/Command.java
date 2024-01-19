19df7138f73e4ca62a296303f9015e727abd9e75
Charles Oliver Nutter <headius@headius.com>
2024-01-19 00:13:05 -0600

    Drop GPL and add licenses to gemspec

--- ext/java/org/jruby/ext/nkf/Command.java
+++ ext/java/org/jruby/ext/nkf/Command.java
@@ -1,5 +1,5 @@
 /***** BEGIN LICENSE BLOCK *****
- * Version: EPL 2.0/GPL 2.0/LGPL 2.1
+ * Version: EPL 2.0/LGPL 2.1
  *
  * The contents of this file are subject to the Eclipse Public
  * License Version 2.0 (the "License"); you may not use this file
@@ -14,16 +14,15 @@
  * Copyright (C) 2011 Koichiro Ohba <koichiro@meadowy.org>
  *
  * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the LGPL are applicable instead
  * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
+ * under the terms of either the LGPL, and not to allow others to
  * use your version of this file under the terms of the EPL, indicate your
  * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
+ * and other provisions required by the LGPL. If you do not delete
  * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the EPL, the GPL or the LGPL.
+ * the terms of any one of the EPL, the LGPL.
  ***** END LICENSE BLOCK *****/
 
 package org.jruby.ext.nkf;
